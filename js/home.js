4967e2cdb06ba4e288c2f11383025627c0f237ed
sujoy-dam <sujooy01@gmail.com>
2024-09-21 14:37:42 +0600

    add money functionality

--- js/home.js
+++ js/home.js
@@ -1,5 +1,18 @@
 console.log('connected')
-document.getElementById('add-money-btn').addEventListener('click', function(){
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
     const amount = document.getElementById('add-money-amount').value;
     const pin = document.getElementById('add-money-pin').value;
+
+    if(pin === '1234'){
+        const totalBalance = document.getElementById('total-balance').innerText;
+        console.log(totalBalance)
+        const totalBanlanceNumber = Number(totalBalance);
+        const amountNumber = Number(amount);
+        const totalBalanceTotal = totalBanlanceNumber + amountNumber;
+        document.getElementById('total-balance').innerText = totalBalanceTotal;
+    }
+    else{
+        alert('Girve the right information')
+    }
 })
