edaf1e7903b7d32e7fd89c1861c4ad98f24f0ff8
sujoy-dam <sujooy01@gmail.com>
2024-09-21 13:52:44 +0600

    home page design

---
+++ js/home.js
@@ -0,0 +1,5 @@
+console.log('connected')
+document.getElementById('add-money-btn').addEventListener('click', function(){
+    const amount = document.getElementById('add-money-amount').value;
+    const pin = document.getElementById('add-money-pin').value;
+})
